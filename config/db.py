12d14ebbb8ba68d6438c3cd759bf31f217ba8491
Patrick <pvilchez794@gmail.com>
2023-03-09 17:44:48 -0500

    Env vars create and requirements.txt export

--- config/db.py
+++ config/db.py
@@ -1,11 +1,17 @@
+import os
 import redis
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 def create_redis() -> redis.ConnectionPool:
     pool = redis.ConnectionPool(
-        host="localhost",
-        port=6379,
-        db=0,
+        username=os.getenv("REDIS_USERNAME"),
+        host=os.getenv("REDIS_HOST"),
+        port=os.getenv("REDIS_PORT"),
+        db=os.getenv("REDIS_DB"),
+        password=os.getenv("REDIS_PASSWORD"),
         decode_responses=True
     )
     return pool
